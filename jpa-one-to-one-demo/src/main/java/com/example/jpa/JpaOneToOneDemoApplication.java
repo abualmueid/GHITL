db597a2a246e64ec5ed3c55995ff08044c3ef69d
Abu Al Mueid <mueid.itsd@goldenharvestbd.com>
2023-03-21 14:48:32 +0600

    modified .gitignore file

---
+++ jpa-one-to-one-demo/src/main/java/com/example/jpa/JpaOneToOneDemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.jpa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToOneDemoApplication {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
