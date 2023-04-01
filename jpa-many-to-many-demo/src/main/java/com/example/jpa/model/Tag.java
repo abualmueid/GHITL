8701ca47c78aebdcd072c786e7e88a37c786984f
Abu Al Mueid <mueid.itsd@goldenharvestbd.com>
2023-04-01 14:37:06 +0600

    added files, modified them and completed the project.

---
+++ jpa-many-to-many-demo/src/main/java/com/example/jpa/model/Tag.java
@@ -0,0 +1,51 @@
+package com.example.jpa.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.NaturalId;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    //@NaturalId
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+                cascade = {CascadeType.PERSIST, CascadeType.MERGE },
+                mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
+
+    public Tag(){
+
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+}
